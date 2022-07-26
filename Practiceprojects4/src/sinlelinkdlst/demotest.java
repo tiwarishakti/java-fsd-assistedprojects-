804f8c31056480543f8cac390ead00c7b11621fe
Shakti Tiwari <shaktitiwari1182511@gmail.com>
2022-07-26 13:32:03 +0530

    changes

---
+++ Practiceprojects4/src/sinlelinkdlst/demotest.java
@@ -0,0 +1,111 @@
+package sinlelinkdlst;
+
+import java.io.*; 
+public class demotest 
+{ 
+Node head; 
+    	static class Node 
+{ 
+        		int data; 
+        		Node next; 
+        		Node(int d) 
+        		{ 
+            			data = d; 
+            			next = null; 
+        		} 
+    	} 
+
+    	public static demotest insert(demotest list, int data) 
+    	{ 
+      
+        		Node new_node = new Node(data); 
+        		new_node.next = null; 
+   	
+        		if (list.head == null) 
+{ 
+            			list.head = new_node; 
+        		} 
+        		else 
+{ 
+          
+            			Node last = list.head;
+            			while (last.next != null) 
+{ 
+                			last = last.next; 
+            			} 
+   			
+            			last.next = new_node; 
+        		} 
+        		return list; 
+    	} 
+   	public static void printList(demotest list) 
+    	{	 
+        		Node currNode = list.head; 
+        		System.out.print("LinkedList: "); 
+        		
+        		while (currNode != null) 
+{ 
+            			 
+            			System.out.print(currNode.data + " "); 
+           
+            			currNode = currNode.next; 
+        		} 
+        		System.out.println(); 
+    	} 
+    	
+    	public static demotest deleteByKey(demotest list, int key) 
+    	{ 
+        
+        		Node currNode = list.head, prev = null; 
+        		if (currNode != null && currNode.data == key) 
+{ 
+            			list.head = currNode.next; 
+            			System.out.println(key + " found and deleted"); 
+            			return list; 
+        		} 
+        		while (currNode != null && currNode.data != key) 
+{ 
+            			prev = currNode; 
+            			currNode = currNode.next; 
+        		} 
+        		if (currNode != null) 
+{ 
+            			prev.next = currNode.next; 
+            			System.out.println(key + " found and deleted"); 
+        		} 
+        		if (currNode == null) 
+{ 
+            			System.out.println(key + " not found"); 
+        		} 
+        		return list; 
+    	} 
+    	
+    	public static void main(String[] args) 
+    	{ 
+        		
+        		demotest list = new demotest(); 
+       
+        		list = insert(list, 1); 
+        		list = insert(list, 2); 
+        		list = insert(list, 3); 
+        		list = insert(list, 4); 
+        		list = insert(list, 5); 
+        		list = insert(list, 6); 
+        		list = insert(list, 7); 
+        		list = insert(list, 8); 
+        
+        		printList(list); 
+        		
+        		deleteByKey(list, 1); 
+  
+        		printList(list); 
+ 
+        		deleteByKey(list, 4); 
+      
+        		printList(list); 
+        		
+       		deleteByKey(list, 10); 
+        
+        		printList(list); 
+    	} 
+} 
